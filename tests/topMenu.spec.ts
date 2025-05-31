00d3313b1c99d91d6736d374f54440cf5b222a28
Mendi45 <caspi45@gmail.com>
2025-05-31 23:14:47 +0300

    New test: Check top menu categories against test data

---
+++ tests/topMenu.spec.ts
@@ -0,0 +1,36 @@
+import { LoginPage } from './../pages/login.page';
+import { test, expect, Browser, BrowserContext, Page } from '@playwright/test';
+import { BasePage } from '../pages/base.page';
+import { TestData } from '../consts/selectors';
+import { secrets } from '../config/secrets';
+
+// Simple comment: Test for top menu categories
+
+test.describe('Top Menu Categories', () => {
+    let browser: Browser;
+    let context: BrowserContext;
+    let page: Page;
+    let basePage: BasePage;
+    let loginPage: LoginPage;
+
+    test.beforeAll(async ({ browser: testBrowser }) => {
+        browser = testBrowser;
+        context = await browser.newContext();
+        page = await context.newPage();
+        basePage = new BasePage(page);
+        loginPage = new LoginPage(page);
+        await basePage.open(secrets.baseUrl);
+    });
+
+    test.afterAll(async () => {
+        await context.close();
+    });
+
+    test('should display all categories in the top menu', async () => {
+        // Get the expected category names from TestData
+        const expectedCategories = Object.values(TestData.categories).map(cat => cat.name);
+
+        const actualMenuItems = await loginPage.getTopMenuItemsText();
+        expect(actualMenuItems).toEqual(expectedCategories);
+    });
+}); 
